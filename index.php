92743849b19f47037822196c56988876b10ec60e
surplusPolyCount <simonhrosenthal@gmail.com>
2021-07-05 22:50:14 -0700

    got all posts displaying

--- index.php
+++ index.php
@@ -5,24 +5,27 @@
     get_header();
 ?>
 <div id="contentwide" class="px-0 justify-content-center m-auto w-full">
-    <div class="row pt-3">  
+    <div class="">  
     <?php 
           //establish variables for new query
           $args = array('post_type'=>'prisoner_submission', 'posts_per_page'=>3);
           //use $args variables to query for the unique post type
           $new_query = new WP_Query($args); 
           //dequeue the first post from the query
-          $new_query->the_post(); 
+         // $new_query->the_post(); 
           //all functions below use properties from dequed post 
           //(even though not explicitly typed)
+          while($new_query -> have_posts() ):$new_query -> the_post();
     ?>
+    <div>
     <h2>
     <?php
         //example: the function on line 23 can be thought of: 
         //print(the_post().the_title()); 
         echo the_title();
         ?></h2>
-    
+
+
 <!-- EXAMPLES: (for source, go to line 445 and below in functions.php)-->
 
     <!-- AUTHOR TERMS --> 
@@ -57,7 +60,8 @@
 
     <!-- PDF TERMS -->
     <a href="<?php echo(get_sub_pdflink($post->ID)); ?>">access the pdf directly through this </a>
-
+    </div> 
+    <?php endwhile; ?>
 </div>
 
-<?php get_footer(); ?>
+<?php   get_footer(); ?>
